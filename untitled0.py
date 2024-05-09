3493492fec717113ac019b62bd275109f2cb9c5c
TACHIKOMA1123 <blackdigiko1123@outlook.com>
2024-05-09 19:40:52 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 18:50:37 2024
+
+@author: USER
+"""
+for a in range((1,10)
+    for b in range(9,1,-1):
+        print(b,"*",a,"=",b*a,sep="",end="\t")
+    print()
